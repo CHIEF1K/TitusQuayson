19f6c9eb32e0d291e9b081538e26d86c97532f4a
Titus Quayson <titusquayson@MacBookAir.localdomain>
2026-01-29 16:21:35 +0200

    Add profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
           <div className="flex justify-center mb-4 sm:mb-6">
             <div className="relative w-20 h-20 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-40 lg:h-40">
               <img 
-                src="/TitusQuayson/profile.jpg" 
+                src="/TitusQuayson/profile.png" 
                 alt="Titus Quayson"
                 className="w-full h-full object-cover rounded-full border-3 sm:border-4 border-cloud-blue shadow-lg"
                 onError={(e) => {
